c00d33376db512a835d8b34920ae1098c649b42d
Emmet Friel <emmetfriel@hotmail.co.uk>
2026-03-09 11:33:13 +0000

    added gsw struct and methods, added traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+// Setup 
+struct GswParameters {
+    
+}
+
+// Secret key 
+struct GswSecretKey {
+
+}
+
+// Public key
+struct GswPublicKey {
+
+}
+
+// Ciphertext
+struct GswCiphertext {
+
+}
+
+// Plaintext
+struct GswPlaintext {
+
+}
+
+// Secret Key generation
+fn secret_key_gen(parameters: GswParameters) -> GswSecretKey {
+    todo!()
+} 
+
+//Public Key generation
+fn public_key_gen(parameters: GswParameters, sk: GswSecretKey) -> GswPublicKey {
+    todo!()
+}
+
+// GSW Encryption
+// u \in Z_q 
+// Outputs matrix \in Zq of size N x N where N = k * l  
+fn encryption(parameters: GswParameters, pk: GswPublicKey, message: u64) -> GswCiphertext {
+    todo!()
+}
+
+// GSW Decryption
+fn decryption(parameters: GswParameters, ct: GswCiphertext, sk: GswSecretKey) -> u64 {
+    todo!()
+}
